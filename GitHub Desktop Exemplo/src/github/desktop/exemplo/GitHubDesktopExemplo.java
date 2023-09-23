5e680252ccc01acb73190b9e1ec8e9e8eba69c0b
hrllacvt <88749605+hrllacvt@users.noreply.github.com>
2023-09-23 18:13:53 -0300

    Hello World
    Added hello world

---
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -0,0 +1,14 @@
+
+package github.desktop.exemplo;
+
+
+public class GitHubDesktopExemplo {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+       
+    }
+    
+}
